9df53e4806897658fc016975621dc4b0bd16f99d
AgustinGarrone <agustinngarrone@hotmail.com>
2022-02-12 00:13:47 -0300

    Agregada vision grid de los resultados. Agregado input de cambio de vista

--- scripts/resultados.js
+++ scripts/resultados.js
@@ -3,10 +3,14 @@ let marcaelegida=""
 nextboton.click(mostrar)
 
 let resultadosMostrados=false
-let guitarrasMostradas=0
+let guitarrasMostradas=0 //! por ahora no lo us ocreo 
 /* let results=document.createElement("section")
 results.classList.add("results")
 results.setAttribute("id", "option6") */
+
+const cambiarVista=document.createElement("div")
+cambiarVista.classList.add("cambiarVista")
+
 
 function mostrar() {             
     switch(index) {
@@ -52,28 +56,30 @@ function mostrar() {
     
 }
 
-
-
 function terminado() {   
     resultadosMostrados=true
+    document.querySelector(".guitarFinderApp").style.display="none"
+    cambiarVista.innerHTML=`<img src="./img/gridicon.png">
+                            <img src="./img/on-slider.png">
+                            <img src="./img/flexicon.png" >`
+    document.querySelector(".guitarFinder").prepend(cambiarVista)
+    document.querySelector(".results").style.display="flex"
+    h2option.innerHTML="This is for YOU :)"
+    document.querySelector(".guitarFinder").prepend(h2option)
     if (elecciones4.length>=0) {
-        document.querySelector(".guitarFinderApp").style.display="none"
-        $(".results").show()
-        h2option.innerHTML="This is for YOU :)"
-        document.querySelector(".guitarFinder").prepend(h2option)
          for (eleccion of elecciones4) {
-             document.querySelector(".splide__list").innerHTML+= ` 
+             document.querySelector(".results__grid").innerHTML+= ` 
              <li class=splide__slide>   
                 <div class=resultsCard100>
                 <span class=resultsCardPorcentaje><p>100%</p></span>
                 <p class=resultsCard100Name>${eleccion.name}</p>
                 <img class=resultsCard100Img src="${eleccion.img}">
-                <p class=resultsCardPrice>${eleccion.price}$</p>
                </div>
             </li>
                `;
             guardarlocal(eleccion.name,JSON.stringify(eleccion))
          }
+    }
      if (elecciones2.length>=0) {
          let pricecoincidence=elecciones2.filter(el => !elecciones3.includes(el));
          console.log("PRICECOINCI ES")
@@ -83,7 +89,7 @@ function terminado() {
                  break
              } */
              if (pricecoincidence[i].brand===marcaelegida) {
-                document.querySelector(".splide__list").innerHTML += `
+                document.querySelector(".results__grid").innerHTML += `
                    <li class=splide__slide>    
                     <div class=resultsCard85 >
                       <span class=resultsCardPorcentaje><p>85%</p></span>
@@ -92,12 +98,11 @@ function terminado() {
                       <div class=resultsCardFail>
                           <p>Price</p><img class=resultsCardFailImg src="./img/cruzroja.png">
                       </div>
-                      <p class=resultsCardPrice>${pricecoincidence[i].price}$</p>
                     </div>
                   </li>
                  `;
              }   else {
-                     document.querySelector(".splide__list").innerHTML += `
+                     document.querySelector(".results__grid").innerHTML += `
                      <li class=splide__slide>       
                      <div class=resultsCard70> 
                        <span class=resultsCardPorcentaje><p>70%</p></span>
@@ -106,14 +111,58 @@ function terminado() {
                        <div class=resultsCardFail>
                            <p>Price and brand</p><img class=resultsCardFailImg src="./img/cruzroja.png">
                        </div>
-                       <p class=resultsCardPrice>${pricecoincidence[i].price}$</p>
                      </div>
                      <li
                       `;
                  }
          }  
      }
+     /* document.querySelector(".resultsCardPorcentaje").addEventListener("mouseenter",function (){
+         alert("Porcentaje de tu eleccion")
+     })*/
+     cargarCardClicks()
+     } 
+    /*  slideron() */
 
-     }
-     slideron()
-}
+
+function cargarCardClicks(){
+    /*  const cards=document.getElementsByClassName("resultsCard100")
+     console.log("CARDS ES")
+     console.log(cards)
+     console.log(elecciones4)
+     for (let i=0;i=cards.length;i++) {
+       console.log(i)
+       conso
+       cards[i].addEventListener("click", function() {
+           document.querySelector(".results__active").innerHTML=` <div class=resultsCardActive>
+                                                                               <span class=resultsCardPorcentaje><p>100%</p></span>
+                                                                               <p class=resultsCardActiveName>${elecciones4[i].name}</p>
+                                                                               <img class=resultsCardActiveImg src="${elecciones4[i].img}">
+                                                                               <div class="resultsCardActive_imgs">
+                                                                                   <img src="${elecciones4[i].img}">
+                                                                                   <img src="${elecciones4[i].img}">
+                                                                                   <img src="${elecciones4[i].img}">
+                                                                               </div> 
+                                                                         </div>` 
+                                                                     }) 
+                                                           
+     }*/
+     let cards=document.querySelectorAll(".resultsCard100")
+     for (let t=0;t<cards.length;t++){
+         cards[t].addEventListener('click', event => {
+             document.querySelector(".results__active").innerHTML=` <div class=resultsCardActive transition-style="in:wipe:down">
+             <div class=backgroundActive transition-style="in:wipe:down">
+             </div>
+             <span class=resultsCardPorcentaje><p>100%</p></span>
+             <p class=resultsCardActiveName>${elecciones4[t].name}</p>
+             <img class=resultsCardActiveImg src="${elecciones4[t].img}">
+             <div class="resultsCardActive_imgs">
+                 <img src="${elecciones4[t].img}">
+                 <img src="${elecciones4[t].img}">
+                 <img src="${elecciones4[t].img}">
+             </div> 
+       </div>` 
+   }) 
+ }
+
+ } 
